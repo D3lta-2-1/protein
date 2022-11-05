7e38d6ccf81ad251dc1d860647de407a9c782149
d3lta <83219809+D3lta-2-1@users.noreply.github.com>
2022-11-05 18:31:05 +0100

    Initial commit

---
+++ src/main/java/me/verya/protein/Protein.java
@@ -0,0 +1,10 @@
+package me.verya.protein;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Protein implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/verya/protein/client/ProteinClient.java
@@ -0,0 +1,13 @@
+package me.verya.protein.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ProteinClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
